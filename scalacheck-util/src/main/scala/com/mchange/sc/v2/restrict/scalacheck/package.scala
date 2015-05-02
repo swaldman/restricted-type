073c205e2383dfbdb8ada7907b44bc98a6d65b93
Steve Waldman <swaldman@mchange.com>
2015-05-02 01:43:23 -0700

    Implemented an implicit decorator offering scalacheck arbitraries for restricted types.

---
+++ scalacheck-util/src/main/scala/com/mchange/sc/v2/restrict/scalacheck/package.scala
@@ -0,0 +1,21 @@
+package com.mchange.sc.v2.restrict;
+
+import org.scalacheck._
+
+package object scalacheck {
+
+  type ShieldType[BELLY] = AnyVal with RestrictedType.Element[BELLY];
+
+  private def gen[BELLY, SHIELD <: ShieldType[BELLY]]( factory : RestrictedType[_, BELLY, SHIELD ] )( implicit arb : Arbitrary[BELLY] ) : Gen[SHIELD] = {
+    val gen : Gen[BELLY] = Arbitrary.arbitrary( arb );
+    gen.filter( _ elem_: factory ).map( factory(_) )
+  }
+
+  private[scalacheck] def arbitrary[BELLY, SHIELD <: ShieldType[BELLY]]( factory : RestrictedType[_, BELLY, SHIELD ] )( implicit arb : Arbitrary[BELLY] ) : Arbitrary[SHIELD] = {
+    Arbitrary( gen( factory )( arb ) )
+  }
+
+  implicit class Decorator[BELLY, SHIELD <: ShieldType[BELLY]]( val factory : RestrictedType[_, BELLY, SHIELD ] ) extends AnyVal {
+    def arbitrary( implicit arb : Arbitrary[BELLY] ) = scalacheck.arbitrary( factory );
+  }
+}
